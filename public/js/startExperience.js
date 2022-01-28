726ea0e2ba8c7883f0f21e208d029de924661db3
vickeychee <vickeychee@cmail.carleton.ca>
2022-01-28 15:23:17 -0500

    added user gesture button

---
+++ public/js/startExperience.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('start-experience',{
+    init: function() {
+        console.log('scene loaded');
+
+        document.querySelector('#user-gesture-button').style.display = 'block';
+        document.querySelector('#loading-animation').style.display = 'none';
+    }
+  });
+
+  const startExperience = function(){
+      //hide user gesture overlay
+      document.querySelector('#user-gesture-overlay').style.display = 'none';
+
+      //start any/all ambient sounds
+      const ambientSounds = document.querySelectorAll('.ambient-music');
+      ambientSounds.forEach(function(soundEntity){
+            soundEntity.components.sound.playSound();
+
+      });
+  }
